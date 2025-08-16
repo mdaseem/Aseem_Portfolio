c26b1712b3571dd12f49e1542bbf0470f7433167
Mohammed Aseem <mohammed.aseem@publicissapient.com>
2025-08-16 23:47:06 +0530

    my portfolio initial changes

--- my-portfolio/src/app/Projects/page.tsx
+++ my-portfolio/src/app/Projects/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import DynamicHoc from "./components/DynamicHoc/DynamicHoc";
-import Header from "./components/Header/Header";
-import Menu from "./components/Menu/Menu";
+import Header from "../components/Header/Header";
+import Menu from "../components/Menu/Menu";
+import DynamicHoc from "../components/DynamicHoc/DynamicHoc";
 
 export default function ThisIsPortfolioHome() {
   return (
     <div className="page-view">
-      <Menu />
       <Header />
       <DynamicHoc />
+      <Menu />
     </div>
   );
 }
