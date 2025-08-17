de9346341298d2b38033719f2661f7c69c9d8653
Mohammed Aseem <mohammed.aseem@publicissapient.com>
2025-08-17 21:04:39 +0530

    my portfolio tiles

--- my-portfolio/src/app/components/Molecules/ProjectsContainer/ProjectContainer.tsx
+++ my-portfolio/src/app/components/Molecules/ProjectsContainer/ProjectContainer.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./ProjectsContainer.css";
 import ProjectTile from "../../Atoms/ProjectTiles/ProjectTile";
 
-
 const dataForTiles = [
   {
     id: 1,
     title: "E-commerce Website",
-    description: "I have worked on an e-commerce website using React and Node.js.",
+    description:
+      "I have worked on an e-commerce website using React and Node.js.",
   },
   {
     id: 2,
@@ -17,14 +17,21 @@ const dataForTiles = [
   {
     id: 3,
     title: "Udemy Clone",
-    description: "I have worked on a Udemy clone using React and Node.js, express, and MongoDB.",
+    description:
+      "I have worked on a Udemy clone using React and Node.js, express, and MongoDB.",
   },
 ];
 
 const ProjectContainer: React.FC = () => {
   return (
-    <div>
-      <div className="project-container-main-content">{dataForTiles.map(tile=><ProjectTile key={tile.id} title={tile.title} description={tile.description} />)}</div>
+    <div className="project-container-main-content">
+      {dataForTiles.map((tile) => (
+        <ProjectTile
+          key={tile.id}
+          title={tile.title}
+          description={tile.description}
+        />
+      ))}
     </div>
   );
 };
